8d48d8386a40a658a0a274bc4e0d8c6f3e127479
Sławomir PIETRAS <slawpie@hotmail.com>
2017-09-09 15:45:10 +0200

    Initial

---
+++ CheckAppVersion/CheckAppVersion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckAppVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckVersion.CheckVersion checkVersion = new CheckVersion.CheckVersion();
+
+            if (checkVersion.NewVersionCheck())
+            {
+                Console.WriteLine("NEW version avaiable !");
+            }
+            else
+            {
+                Console.WriteLine("NO new version !");
+                Console.WriteLine(checkVersion.ErrorMessage);
+            }
+        }
+    }
+}
---
+++ CheckAppVersion/CheckVersion/CheckVersion.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.Xml.XPath;
+
+namespace CheckVersion
+{
+    public class CheckVersion
+    {
+        private Version newVersion = null;
+        private string errorMessage;
+
+        public string ErrorMessage
+        {
+            get
+            {
+                return errorMessage;
+            }
+            set
+            {
+                errorMessage = value;
+            }
+        }
+
+
+        //current version of application 
+        private Version currentVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+
+        //url assigned to current applications versions
+        private string url = @"http://domain/versions.xml";
+
+        //application name
+        private string applicationName = "ApplicationTwo";
+
+        // return number of new version
+        private Version checkVersion()
+        {
+            if (checkXmlUrl())
+            {
+                try
+                {
+                    XPathDocument oXPathDocument = new XPathDocument(url);
+                    XPathNavigator oXPathNavigator = oXPathDocument.CreateNavigator();
+                    XPathNodeIterator oApplicationNodesIterator = oXPathNavigator.Select("/Applications/Application");
+
+                    foreach (XPathNavigator oCurrentApplication in oApplicationNodesIterator)
+                    {
+                        if (oCurrentApplication.SelectSingleNode("Name").Value == applicationName)
+                        {
+                            newVersion = new Version(oCurrentApplication.SelectSingleNode("Version").Value);
+                        }
+                    }
+                }
+                catch (Exception oException)
+                {
+                    errorMessage = oException.Message;
+                }
+            }
+            return newVersion;
+        }
+
+
+        //check if url with new version exixsts
+        private bool checkXmlUrl()
+        {
+            try
+            {
+                var request = WebRequest.Create(url);
+                var response = request.GetResponse();
+                return true;
+            }
+            catch (WebException we)
+            {
+                errorMessage = "URL not exixts";
+            }
+            return false;
+        }
+
+
+        //compare versions
+        public bool NewVersionCheck()
+        {
+            checkVersion();
+            if (currentVersion.CompareTo(newVersion) < 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
